5c79ff7dfbddc36fe26a2b8d585fbf6fd24d84d7
tz1006 <a@omg.tf>
2018-03-04 20:09:59 -0800

    Create stock.py

---
+++ stock.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*- 
+# filename: stock.py
+
+from tools import *
+
+class stock():
+    def __init__(self, code):
+        self.code = code
+    # 价格
+    def price(self):
+        return price_now(self.code)
+    # 涨幅
+    def wave(self):
+        return price_now(self.code)
+    def t():
+
+
+
+
+
+sh600123 = stock('600123')
+sh600123.price
