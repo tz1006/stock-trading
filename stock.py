3bef90db0a900594f1d45895696458e7e2bf311b
tz1006 <a@omg.tf>
2018-03-10 09:43:28 -0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -8,16 +8,21 @@ class stock():
     def __init__(self, code):
         self.code = code
         self.name = stock_name(code)
-        self.open_price = 0
+    # 停牌
+    def status():
+    	pass
     # 价格
     def price(self):
         return price_now(self.code)
     # 涨幅
     def wave(self):
         return price_now(self.code)
-    def t():
-
-
+    # MA
+    def MA():
+    	return MA
+    # 历史价格
+    def history():
+    	pass
 
 
 
